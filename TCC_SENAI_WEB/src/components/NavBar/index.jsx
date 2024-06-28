0b0ff245fc4900269408497a84a39ec37bb8f5c1
luizblank <luizrosacardoso@gmail.com>
2024-06-28 10:39:14 -0300

    Code/Files organization and added NavBar component

---
+++ TCC_SENAI_WEB/src/components/NavBar/index.jsx
@@ -0,0 +1,12 @@
+import styles from './styles.module.scss';
+
+export default function NavBar() {
+    return (
+        <>
+            <div className={styles.boschcolors}/>
+            <div className={styles.navbar}>
+                <img src='boschlogo.png' className={styles.boschlogo}/>
+            </div>
+        </>
+    )
+}
--- TCC_SENAI_WEB/src/main.jsx
+++ TCC_SENAI_WEB/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+import NavBar from "./components/NavBar";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <></>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <NavBar/>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
