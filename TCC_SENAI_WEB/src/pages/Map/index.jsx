db19c878091408235d7530a7fd9f41a5496cf1aa
luizblank <luizrosacardoso@gmail.com>
2024-07-25 11:12:11 -0300

    Added buttons to Map page

--- TCC_SENAI_WEB/src/pages/Map/index.jsx
+++ TCC_SENAI_WEB/src/pages/Map/index.jsx
@@ -7,6 +7,13 @@ export default function Map() {
                 <div className={styles.col}>
                     <h1>Setores</h1>
                     <img className={styles.plant} src='planta.png'/>
+                    <button className={styles.ct101} id='ct101' name='ct101'>101</button>
+                    <button className={styles.ct201} id='ct201' name='ct201'>201</button>
+                    <button className={styles.ct210} id='ct210' name='ct210'>210</button>
+                    <button className={styles.ct303} id='ct303' name='ct303'>303</button>
+                    <button className={styles.ct304} id='ct304' name='ct304'>304</button>
+                    <button className={styles.ct401} id='ct401' name='ct401'>401</button>
+                    <button className={styles.ct401a} id='ct401a' name='ct401a'>401-A</button>
                 </div>
             </div>
         </>
